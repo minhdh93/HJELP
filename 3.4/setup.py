6d66fe4e8bf47e6becf24cb8054e2a1ff5bf6bf8
Minh Duong <minhdh93@hotmail.com>
2016-09-15 00:27:58 +0200

    remove folder

--- 3.4/setup.py
+++ 3.4/setup.py
@@ -4,5 +4,5 @@ name='my_unit_testing'
 setup(name=name,
       version='0.1',
       py_modules=[name],       # modules to be installed
-      scripts=['./my_unit_testing/' + name + '.py'],  # programs to be installed
+      scripts=[name + '.py'],  # programs to be installed
       )
